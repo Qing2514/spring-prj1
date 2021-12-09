90356690700eef8b08e65cc0730a2c6b6928fa1b
Qing2514 <2514632453@qq.com>
2021-12-09 13:29:29 +0800

    first commit

---
+++ src/main/java/cn/edu/zjut/dao/CustomerDAO.java
@@ -0,0 +1,36 @@
+package cn.edu.zjut.dao;
+
+import cn.edu.zjut.po.Customer;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class CustomerDAO extends BaseHibernateDAO implements ICustomerDAO
+{
+    public CustomerDAO()
+    {
+        System.out.println("create CustomerDao.");
+    }
+
+    public void save(Customer transientInstance)
+    {
+        Transaction tran = null;
+        Session session = null;
+        try
+        {
+            session = getSession();
+            tran = session.beginTransaction();
+            session.save(transientInstance);
+            tran.commit();
+        }
+        catch (RuntimeException re)
+        {
+            if (tran != null) tran.rollback();
+            throw re;
+        }
+        finally
+        {
+            session.close();
+        }
+    }
+}
---
+++ src/main/java/cn/edu/zjut/dao/ICustomerDAO.java
@@ -0,0 +1,8 @@
+package cn.edu.zjut.dao;
+
+import cn.edu.zjut.po.Customer;
+
+public interface ICustomerDAO
+{
+    public void save(Customer transientInstance);
+}
